68b052ea7cbf9f74275a090cff6a81fa092cd65e
Laroussi Nouha <119297522+NouhaLr@users.noreply.github.com>
2025-06-03 07:21:10 +0200

    Initial commit

--- myapp/schema.py
+++ myapp/schema.py
@@ -4,10 +4,25 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+# Type GraphQL pour User
 class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = ("id", "username", "email")
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, username, email, password):
+        user = User(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
 
 class Query(graphene.ObjectType):
     all_users = graphene.List(UserType)
@@ -15,4 +30,8 @@ class Query(graphene.ObjectType):
     def resolve_all_users(root, info):
         return User.objects.all()
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
